00ddb180d6e1cb34cbc2ecc59bf436e6cc9460ca
nadia <nnaji@conga.com>
2022-02-28 10:36:24 -0700

    Create BoulderAttemptTests class

---
+++ ClimbCalculator.Tests/Routes/BoulderAttemptTests.cs
@@ -0,0 +1,12 @@
+using ClimbCalculator.Routes;
+
+namespace ClimbCalculator.Tests.Routes;
+
+public class BoulderAttemptTests
+{
+    private static Boulderer[] FakeBoulderers = new Boulder[2]
+    {
+        new Boulderer("Nadia", 9, 7),
+        
+    }
+}
